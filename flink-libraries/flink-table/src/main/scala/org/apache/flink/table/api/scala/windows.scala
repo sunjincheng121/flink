surli/backup-experiments

[FLINK-6228][table] Integrating the OVER windows in the Table API (stream)

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/scala/windows.scala
@@ -19,7 +19,7 @@
 package org.apache.flink.table.api.scala
 
 import org.apache.flink.table.expressions.Expression
-import org.apache.flink.table.api.{SessionWindow, SlideWithSize, TumblingWindow}
+import org.apache.flink.table.api.{SessionWindow, SlideWithSize, TumblingWindow, OverWindow}
 
 /**
   * Helper object for creating a tumbling window. Tumbling windows are consecutive, non-overlapping
@@ -82,4 +82,50 @@ object Session {
     * @return a session window
     */
   def withGap(gap: Expression): SessionWindow = new SessionWindow(gap)
+}
+
+/**
+  * Helper object for creating a over window.
+  */
+object Over {
+
+  /**
+    * Specifies the time attribute on which rows are grouped.
+    *
+    * For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]] to specify time mode.
+    *
+    * For batch tables, refer to a timestamp or long attribute.
+    */
+  def orderBy(orderBy: Expression): OverWindow = {
+    val overWindow = new OverWindow
+    overWindow.orderBy = orderBy
+    overWindow
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy some partition keys.
+    * @return A partitionedOver instance that only contains the orderBy method.
+    */
+  def partitionBy(partitionBy: Expression*): PartitionedOver = {
+    PartitionedOver(partitionBy: _*)
+  }
+}
+
+case class PartitionedOver(partitionBy: Expression*) {
+
+  /**
+    * Specifies the time attribute on which rows are grouped.
+    *
+    * For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]] to specify time mode.
+    *
+    * For batch tables, refer to a timestamp or long attribute.
+    */
+  def orderBy(orderBy: Expression): OverWindow = {
+    val overWindow = new OverWindow
+    overWindow.orderBy = orderBy
+    overWindow.partitionBy = partitionBy
+    overWindow
+  }
 }

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/windows.scala
@@ -22,6 +22,126 @@ import org.apache.flink.table.expressions.{Expression, ExpressionParser}
 import org.apache.flink.table.plan.logical._
 
 /**
+  * An over window specification.
+  *
+  * Over window is similar to the traditional OVER SQL.
+  */
+class OverWindow {
+
+  private[flink] var alias: Expression = _
+  private[flink] var partitionBy: Seq[Expression] = Seq[Expression]()
+  private[flink] var orderBy: Expression = _
+  private[flink] var preceding: Expression = _
+  private[flink] var following: Expression = null
+
+  /**
+    * Assigns an alias for this window that the following `select()` clause can refer to.
+    *
+    * @param alias alias for this over window
+    * @return this over window
+    */
+  def as(alias: String): OverWindow = as(ExpressionParser.parseExpression(alias))
+
+  /**
+    * Assigns an alias for this window that the following `select()` clause can refer to.
+    *
+    * @param alias alias for this over window
+    * @return this over window
+    */
+  def as(alias: Expression): OverWindow = {
+    this.alias = alias
+    this
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy
+    * @return this over window
+    */
+  def partitionBy(partitionBy: String): OverWindow = {
+    this.partitionBy(ExpressionParser.parseExpression(partitionBy))
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy
+    * @return this over window
+    */
+  def partitionBy(partitionBy: Expression*): OverWindow = {
+    this.partitionBy = partitionBy
+    this
+  }
+
+
+  /**
+    * Specifies the time mode.
+    *
+    * @param orderBy For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]]
+    *                to specify time mode.
+    * @return this over window
+    */
+  def orderBy(orderBy: String): OverWindow = {
+    this.orderBy(ExpressionParser.parseExpression(orderBy))
+  }
+
+  /**
+    * Specifies the time mode.
+    *
+    * @param orderBy For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]]
+    *                to specify time mode.
+    * @return this over window
+    */
+  def orderBy(orderBy: Expression): OverWindow = {
+    this.orderBy = orderBy
+    this
+  }
+
+  /**
+    * Set the preceding offset (based on time or row-count intervals) for over window
+    *
+    * @param preceding forward offset that relative to the current row
+    * @return this over window
+    */
+  def preceding(preceding: String): OverWindow = {
+    this.preceding(ExpressionParser.parseExpression(preceding))
+  }
+
+  /**
+    * Set the preceding offset (based on time or row-count intervals) for over window
+    *
+    * @param preceding forward offset that relative to the current row
+    * @return this over window
+    */
+  def preceding(preceding: Expression): OverWindow = {
+    this.preceding = preceding
+    this
+  }
+
+  /**
+    * Set the following offset (based on time or row-count intervals) for over window
+    *
+    * @param following subsequent offset that relative to the current row
+    * @return this over window
+    */
+  def following(following: String): OverWindow = {
+    this.following(ExpressionParser.parseExpression(following))
+  }
+
+  /**
+    * Set the following offset (based on time or row-count intervals) for over window
+    *
+    * @param following subsequent offset that relative to the current row
+    * @return this over window
+    */
+  def following(following: Expression): OverWindow = {
+    this.following = following
+    this
+  }
+}
+
+/**
   * A window specification.
   *
   * Window groups rows based on time or row-count intervals. It is a general way to group the

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/expressions/call.scala
@@ -17,15 +17,23 @@
  */
 package org.apache.flink.table.expressions
 
-import org.apache.calcite.rex.RexNode
+import java.util
+
+import com.google.common.collect.ImmutableList
+import org.apache.calcite.rex.RexWindowBound._
+import org.apache.calcite.rex.{RexFieldCollation, RexNode, RexWindowBound}
+import org.apache.calcite.sql._
+import org.apache.calcite.sql.`type`.{BasicSqlType, SqlTypeName, OrdinalReturnTypeInference}
+import org.apache.calcite.sql.parser.SqlParserPos
 import org.apache.calcite.tools.RelBuilder
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.table.api.{UnresolvedException, ValidationException}
+import org.apache.flink.table.api.{OverWindow, UnresolvedException, ValidationException}
 import org.apache.flink.table.calcite.FlinkTypeFactory
 import org.apache.flink.table.functions.utils.UserDefinedFunctionUtils._
 import org.apache.flink.table.functions.{ScalarFunction, TableFunction}
 import org.apache.flink.table.plan.logical.{LogicalNode, LogicalTableFunctionCall}
 import org.apache.flink.table.validate.{ValidationFailure, ValidationResult, ValidationSuccess}
+import org.apache.flink.table.typeutils.{RowIntervalTypeInfo, SqlTypeUtils}
 
 /**
   * General expression for unresolved function calls. The function can be a built-in
@@ -46,6 +54,128 @@ case class Call(functionName: String, args: Seq[Expression]) extends Expression 
 
   override private[flink] def validateInput(): ValidationResult =
     ValidationFailure(s"Unresolved function call: $functionName")
+}
+
+/**
+  * Over expression for calcite over transform.
+  *
+  * @param agg             over-agg expression
+  * @param aggAlias        agg alias for following `select()` clause.
+  * @param overWindowAlias over window alias
+  * @param overWindow      over window
+  */
+case class OverCall(
+    agg: Aggregation,
+    var aggAlias: Expression,
+    overWindowAlias: Expression,
+    var overWindow: OverWindow = null) extends Expression {
+
+  private[flink] def as(aggAlias: Expression): OverCall = {
+    this.aggAlias = aggAlias
+    this
+  }
+
+  override private[flink] def toRexNode(implicit relBuilder: RelBuilder): RexNode = {
+
+    val rexBuilder = relBuilder.getRexBuilder
+
+    val operator: SqlAggFunction = agg.toSqlAggFunction()
+
+    val aggReturnType: TypeInformation[_] = agg.resultType
+
+    val relDataType = SqlTypeUtils.createSqlType(relBuilder.getTypeFactory, aggReturnType)
+
+    val aggExprs: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+    val aggChildName = agg.child.asInstanceOf[ResolvedFieldReference].name
+
+    aggExprs.add(relBuilder.field(aggChildName))
+
+    val orderKeys: ImmutableList.Builder[RexFieldCollation] =
+      new ImmutableList.Builder[RexFieldCollation]()
+
+    val sets:util.HashSet[SqlKind] = new util.HashSet[SqlKind]()
+    val orderName = overWindow.orderBy.asInstanceOf[UnresolvedFieldReference].name
+
+    val rexNode =
+      if (orderName.equalsIgnoreCase("rowtime") || orderName.equalsIgnoreCase("proctime")) {
+        // for stream
+        relBuilder.literal(orderName)
+      } else {
+        // for batch
+        relBuilder.field(orderName)
+      }
+
+    orderKeys.add(new RexFieldCollation(rexNode,sets))
+
+    val partitionKeys: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+    overWindow.partitionBy.foreach(x=>
+      partitionKeys.add(relBuilder.field(x.asInstanceOf[UnresolvedFieldReference].name)))
+
+    val preceding = overWindow.preceding.asInstanceOf[Literal]
+    val following = overWindow.following.asInstanceOf[Literal]
+
+    val isPhysical: Boolean = preceding.resultType.isInstanceOf[RowIntervalTypeInfo]
+
+    val lowerBound = createBound(relBuilder, preceding.value.asInstanceOf[Long], SqlKind.PRECEDING)
+    val upperBound = createBound(relBuilder, following.value.asInstanceOf[Long], SqlKind.FOLLOWING)
+
+    rexBuilder.makeOver(
+      relDataType,
+      operator,
+      aggExprs,
+      partitionKeys,
+      orderKeys.build,
+      lowerBound,
+      upperBound,
+      isPhysical,
+      true,
+      false)
+  }
+
+  private def createBound(
+    relBuilder: RelBuilder,
+    precedingValue: Long,
+    sqlKind: SqlKind): RexWindowBound = {
+
+    if (precedingValue == Long.MaxValue) {
+      val unbounded = SqlWindow.createUnboundedPreceding(SqlParserPos.ZERO)
+      create(unbounded, null)
+    } else if (precedingValue == 0L) {
+      val currentRow = SqlWindow.createCurrentRow(SqlParserPos.ZERO)
+      create(currentRow, null)
+    } else {
+
+      val returnType = new BasicSqlType(
+        relBuilder.getTypeFactory.getTypeSystem,
+        SqlTypeName.DECIMAL)
+
+      val sqlOperator = new SqlPostfixOperator(
+        sqlKind.name,
+        sqlKind,
+        2,
+        new OrdinalReturnTypeInference(0),
+        null,
+        null)
+
+      val operands: Array[SqlNode] = new Array[SqlNode](1)
+      operands(0) = (SqlLiteral.createExactNumeric("1", SqlParserPos.ZERO))
+
+      val node = new SqlBasicCall(sqlOperator, operands, SqlParserPos.ZERO)
+
+      val expressions: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+      expressions.add(relBuilder.literal(precedingValue))
+
+      val rexNode = relBuilder.getRexBuilder.makeCall(returnType, sqlOperator, expressions)
+
+      create(node, rexNode)
+    }
+  }
+
+  override private[flink] def children: Seq[Expression] = Seq()
+
+  override def toString = s"${this.getClass.getCanonicalName}(${overWindowAlias.toString})"
+
+  override private[flink] def resultType = agg.resultType
 }
 
 /**

sunjincheng121/flink

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/scala/windows.scala
@@ -19,7 +19,7 @@
 package org.apache.flink.table.api.scala
 
 import org.apache.flink.table.expressions.Expression
-import org.apache.flink.table.api.{SessionWindow, SlideWithSize, TumblingWindow}
+import org.apache.flink.table.api.{SessionWindow, SlideWithSize, TumblingWindow, OverWindowPredefined}
 
 /**
   * Helper object for creating a tumbling window. Tumbling windows are consecutive, non-overlapping
@@ -82,4 +82,50 @@ object Session {
     * @return a session window
     */
   def withGap(gap: Expression): SessionWindow = new SessionWindow(gap)
+}
+
+/**
+  * Helper object for creating a over window.
+  */
+object Over {
+
+  /**
+    * Specifies the time attribute on which rows are grouped.
+    *
+    * For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]] to specify time mode.
+    *
+    * For batch tables, refer to a timestamp or long attribute.
+    */
+  def orderBy(orderBy: Expression): OverWindowPredefined = {
+    val overWindow = new OverWindowPredefined
+    overWindow.orderBy = orderBy
+    overWindow
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy some partition keys.
+    * @return A partitionedOver instance that only contains the orderBy method.
+    */
+  def partitionBy(partitionBy: Expression*): PartitionedOver = {
+    PartitionedOver(partitionBy: _*)
+  }
+}
+
+case class PartitionedOver(partitionBy: Expression*) {
+
+  /**
+    * Specifies the time attribute on which rows are grouped.
+    *
+    * For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]] to specify time mode.
+    *
+    * For batch tables, refer to a timestamp or long attribute.
+    */
+  def orderBy(orderBy: Expression): OverWindowPredefined = {
+    val overWindow = new OverWindowPredefined
+    overWindow.orderBy = orderBy
+    overWindow.partitionBy = partitionBy
+    overWindow
+  }
 }

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/windows.scala
@@ -20,6 +20,143 @@ package org.apache.flink.table.api
 
 import org.apache.flink.table.expressions.{Expression, ExpressionParser}
 import org.apache.flink.table.plan.logical._
+import org.apache.flink.table.typeutils.RowIntervalTypeInfo
+import org.apache.flink.table.api.scala.{CURRENT_RANGE, CURRENT_ROW}
+
+/**
+  * Over window is similar to the traditional OVER SQL.
+  */
+case class OverWindow(
+    alias: Expression,
+    partitionBy: Seq[Expression],
+    orderBy: Expression,
+    preceding: Expression,
+    following: Expression)
+
+/**
+  * An over window predefined  specification.
+  */
+class OverWindowPredefined {
+  private[flink] var partitionBy: Seq[Expression] = Seq[Expression]()
+  private[flink] var orderBy: Expression = _
+  private[flink] var preceding: Expression = _
+  private[flink] var following: Expression = null
+
+  /**
+    * Assigns an alias for this window that the following `select()` clause can refer to.
+    *
+    * @param alias alias for this over window
+    * @return over window
+    */
+  def as(alias: String): OverWindow = as(ExpressionParser.parseExpression(alias))
+
+  /**
+    * Assigns an alias for this window that the following `select()` clause can refer to.
+    *
+    * @param alias alias for this over window
+    * @return over window
+    */
+  def as(alias: Expression): OverWindow = {
+
+    // we want return the complete OverWindow,
+    // so we check the Optional members (`following` and `pratitionBy`) value here
+    if (null == following) {
+      if (preceding.resultType.isInstanceOf[RowIntervalTypeInfo]) {
+        following = CURRENT_ROW
+      } else {
+        following = CURRENT_RANGE
+      }
+    }
+    OverWindow(alias, partitionBy, orderBy, preceding, following)
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy
+    * @return this over window
+    */
+  def partitionBy(partitionBy: String): OverWindowPredefined = {
+    this.partitionBy(ExpressionParser.parseExpression(partitionBy))
+  }
+
+  /**
+    * Partitions the elements on some partition keys.
+    *
+    * @param partitionBy
+    * @return this over window
+    */
+  def partitionBy(partitionBy: Expression*): OverWindowPredefined = {
+    this.partitionBy = partitionBy
+    this
+  }
+
+
+  /**
+    * Specifies the time mode.
+    *
+    * @param orderBy For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]]
+    *                to specify time mode.
+    * @return this over window
+    */
+  def orderBy(orderBy: String): OverWindowPredefined = {
+    this.orderBy(ExpressionParser.parseExpression(orderBy))
+  }
+
+  /**
+    * Specifies the time mode.
+    *
+    * @param orderBy For streaming tables call [[orderBy 'rowtime or orderBy 'proctime]]
+    *                to specify time mode.
+    * @return this over window
+    */
+  def orderBy(orderBy: Expression): OverWindowPredefined = {
+    this.orderBy = orderBy
+    this
+  }
+
+  /**
+    * Set the preceding offset (based on time or row-count intervals) for over window
+    *
+    * @param preceding forward offset that relative to the current row
+    * @return this over window
+    */
+  def preceding(preceding: String): OverWindowPredefined = {
+    this.preceding(ExpressionParser.parseExpression(preceding))
+  }
+
+  /**
+    * Set the preceding offset (based on time or row-count intervals) for over window
+    *
+    * @param preceding forward offset that relative to the current row
+    * @return this over window
+    */
+  def preceding(preceding: Expression): OverWindowPredefined = {
+    this.preceding = preceding
+    this
+  }
+
+  /**
+    * Set the following offset (based on time or row-count intervals) for over window
+    *
+    * @param following subsequent offset that relative to the current row
+    * @return this over window
+    */
+  def following(following: String): OverWindowPredefined = {
+    this.following(ExpressionParser.parseExpression(following))
+  }
+
+  /**
+    * Set the following offset (based on time or row-count intervals) for over window
+    *
+    * @param following subsequent offset that relative to the current row
+    * @return this over window
+    */
+  def following(following: Expression): OverWindowPredefined = {
+    this.following = following
+    this
+  }
+}
 
 /**
   * A window specification.

flink-libraries/flink-table/src/main/scala/org/apache/flink/table/expressions/call.scala
@@ -17,16 +17,26 @@
  */
 package org.apache.flink.table.expressions
 
-import org.apache.calcite.rex.RexNode
+import java.util
+
+import com.google.common.collect.ImmutableList
+import org.apache.calcite.rex.RexWindowBound._
+import org.apache.calcite.rex.{RexFieldCollation, RexNode, RexWindowBound}
+import org.apache.calcite.sql._
+import org.apache.calcite.sql.`type`.OrdinalReturnTypeInference
+import org.apache.calcite.sql.parser.SqlParserPos
 import org.apache.calcite.tools.RelBuilder
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.table.api.{UnresolvedException, ValidationException}
+import org.apache.flink.table.api.{OverWindow, UnresolvedException, ValidationException}
 import org.apache.flink.table.calcite.FlinkTypeFactory
 import org.apache.flink.table.functions.utils.UserDefinedFunctionUtils._
 import org.apache.flink.table.functions.{ScalarFunction, TableFunction}
 import org.apache.flink.table.plan.logical.{LogicalNode, LogicalTableFunctionCall}
 import org.apache.flink.table.validate.{ValidationFailure, ValidationResult, ValidationSuccess}
-
+import org.apache.flink.table.typeutils.RowIntervalTypeInfo
+import org.apache.flink.table.api.Types
+import org.apache.flink.table.api.scala.{UNBOUNDED_RANGE, UNBOUNDED_ROW, CURRENT_RANGE, CURRENT_ROW}
+import org.apache.flink.table.functions.{EventTimeExtractor, ProcTimeExtractor}
 /**
   * General expression for unresolved function calls. The function can be a built-in
   * scalar function or a user-defined scalar function.
@@ -46,6 +56,160 @@ case class Call(functionName: String, args: Seq[Expression]) extends Expression 
 
   override private[flink] def validateInput(): ValidationResult =
     ValidationFailure(s"Unresolved function call: $functionName")
+}
+
+/**
+  * Over expression for calcite over transform.
+  *
+  * @param agg             over-agg expression
+  * @param overWindowAlias over window alias
+  * @param overWindow      over window
+  */
+case class OverCall(
+    agg: Aggregation,
+    overWindowAlias: Expression,
+    var overWindow: OverWindow = null) extends Expression {
+
+  override private[flink] def toRexNode(implicit relBuilder: RelBuilder): RexNode = {
+
+    val rexBuilder = relBuilder.getRexBuilder
+
+    val operator: SqlAggFunction = agg.getSqlAggFunction()
+
+    val relDataType = relBuilder
+      .getTypeFactory.asInstanceOf[FlinkTypeFactory]
+      .createTypeFromTypeInfo(agg.resultType)
+
+    val aggExprs: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+    val aggChildName = agg.child.asInstanceOf[ResolvedFieldReference].name
+
+    aggExprs.add(relBuilder.field(aggChildName))
+
+    val orderKeys: ImmutableList.Builder[RexFieldCollation] =
+      new ImmutableList.Builder[RexFieldCollation]()
+
+    val sets: util.HashSet[SqlKind] = new util.HashSet[SqlKind]()
+    val orderName = overWindow.orderBy.asInstanceOf[UnresolvedFieldReference].name
+
+    val rexNode =
+      if (orderName.equalsIgnoreCase("rowtime")) {
+        // for stream event-time
+        relBuilder.call(EventTimeExtractor)
+      }
+      else if (orderName.equalsIgnoreCase("proctime")) {
+        // for stream proc-time
+        relBuilder.call(ProcTimeExtractor)
+      } else {
+        // for batch event-time
+        relBuilder.field(orderName)
+      }
+
+    orderKeys.add(new RexFieldCollation(rexNode, sets))
+
+    val partitionKeys: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+    overWindow.partitionBy.foreach {
+      x =>
+        val partitionKey = relBuilder.field(x.asInstanceOf[UnresolvedFieldReference].name)
+        if (!FlinkTypeFactory.toTypeInfo(partitionKey.getType).isKeyType) {
+          throw ValidationException(
+            s"expression $partitionKey cannot be used as a partition key expression " +
+            "because it's not a valid key type which must be hashable and comparable")
+        }
+        partitionKeys.add(partitionKey)
+    }
+
+    val preceding = overWindow.preceding.asInstanceOf[Literal]
+    val following = overWindow.following.asInstanceOf[Literal]
+
+    val isPhysical: Boolean = preceding.resultType.isInstanceOf[RowIntervalTypeInfo]
+
+    val lowerBound = createBound(relBuilder, preceding, SqlKind.PRECEDING)
+    val upperBound = createBound(relBuilder, following, SqlKind.FOLLOWING)
+
+    rexBuilder.makeOver(
+      relDataType,
+      operator,
+      aggExprs,
+      partitionKeys,
+      orderKeys.build,
+      lowerBound,
+      upperBound,
+      isPhysical,
+      true,
+      false)
+  }
+
+  private def createBound(
+    relBuilder: RelBuilder,
+    bound: Literal,
+    sqlKind: SqlKind): RexWindowBound = {
+
+    if (bound == UNBOUNDED_RANGE || bound == UNBOUNDED_ROW) {
+      val unbounded = SqlWindow.createUnboundedPreceding(SqlParserPos.ZERO)
+      create(unbounded, null)
+    } else if (bound == CURRENT_RANGE || bound == CURRENT_ROW) {
+      val currentRow = SqlWindow.createCurrentRow(SqlParserPos.ZERO)
+      create(currentRow, null)
+    } else {
+      val returnType = relBuilder
+        .getTypeFactory.asInstanceOf[FlinkTypeFactory]
+        .createTypeFromTypeInfo(Types.DECIMAL)
+
+      val sqlOperator = new SqlPostfixOperator(
+        sqlKind.name,
+        sqlKind,
+        2,
+        new OrdinalReturnTypeInference(0),
+        null,
+        null)
+
+      val operands: Array[SqlNode] = new Array[SqlNode](1)
+      operands(0) = (SqlLiteral.createExactNumeric("1", SqlParserPos.ZERO))
+
+      val node = new SqlBasicCall(sqlOperator, operands, SqlParserPos.ZERO)
+
+      val expressions: util.ArrayList[RexNode] = new util.ArrayList[RexNode]()
+      expressions.add(relBuilder.literal(bound.value))
+
+      val rexNode = relBuilder.getRexBuilder.makeCall(returnType, sqlOperator, expressions)
+
+      create(node, rexNode)
+    }
+  }
+
+  override private[flink] def children: Seq[Expression] = Seq(agg)
+
+  override def toString = s"${this.getClass.getCanonicalName}(${overWindowAlias.toString})"
+
+  override private[flink] def resultType = agg.resultType
+
+  override private[flink] def validateInput(): ValidationResult = {
+    var validationResult: ValidationResult = ValidationSuccess
+    val orderName = overWindow.orderBy.asInstanceOf[UnresolvedFieldReference].name
+    if (!orderName.equalsIgnoreCase("rowtime")
+      && !orderName.equalsIgnoreCase("proctime")) {
+      ValidationFailure(
+        s"OrderBy expression must be ['rowtime] or ['proctime], but got ['${orderName}]")
+    }
+    if (!overWindow.preceding.asInstanceOf[Literal].resultType.getClass
+         .equals(overWindow.following.asInstanceOf[Literal].resultType.getClass)) {
+      ValidationFailure(
+        "Proceeding and the following must be based on same intervals type (time or row-count).")
+    }
+
+    val precedingValue = overWindow.preceding.asInstanceOf[Literal].value.asInstanceOf[Long]
+    if (precedingValue <= 0) {
+      validationResult = ValidationFailure(
+        s"Proceeding value is [${precedingValue}], It should be bigger than 0.")
+    }
+
+    val followingValue = overWindow.following.asInstanceOf[Literal].value.asInstanceOf[Long]
+    if (followingValue < -1) {
+      validationResult = ValidationFailure(
+        s"Following value is [${followingValue}], It should be bigger than -1.")
+    }
+    validationResult
+  }
 }
 
 /**